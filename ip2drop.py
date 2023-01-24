b89879f032eaf6c6a2dbc0150bbd07f1c8c514e9
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-01-24 17:08:35 +0600

    + add validation logic

--- ip2drop.py
+++ ip2drop.py
@@ -4,7 +4,6 @@ import os
 import re
 import sqlite3
 import ipaddress
-import iptools
 from collections import Counter
 import datetime
 import argparse
@@ -122,11 +121,11 @@ def extract_ip(line):
     ip = pattern.search(line)[0]
     return ip
 
-def validate_ip(ip):
-    # https://stackoverflow.com/questions/3462784/check-if-a-string-matches-an-ip-address-pattern-in-python
-    iptools.ipv4.validate_ip(ip) #returns bool
-    # iptools.ipv6.validate_ip(ipv6) #returns bool
-    # TODO: need to add validation logic
+# def validate_ip(ip):
+#     # https://stackoverflow.com/questions/3462784/check-if-a-string-matches-an-ip-address-pattern-in-python
+#     iptools.ipv4.validate_ip(ip) #returns bool
+#     # iptools.ipv6.validate_ip(ipv6) #returns bool
+#     # TODO: need to add validation logic
 
 def delete_ip(ip):
     if ip_exist(ip):
