6cb7fad4e440b4fb638810a2e8f5fda7d5bb6eb0
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-04-11 17:10:49 +0600

    + get info from ip (drop date, timeout, delta

--- helper.py
+++ helper.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import sqlite3
 import sys
+from datetime import datetime
 from pathlib import Path
 
 sys.path.append(str(Path(sys.argv[0]).absolute().parent.parent))
@@ -22,6 +23,9 @@ CONFIG = var.CONFIG
 lib.msg_info(f'Loaded config: {var.LOADED_CONFIG}')
 lib.msg_info(f'Server mode: {SERVER_MODE}')
 lib.msg_info(f'Config DB: {var.DROP_DB}')
+
+# Datetime Format for Journalctl exported logs
+DATETIME_DEFAULT_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
 # Arguments parser section
 # ------------------------------------------------------------------------------------------------------/
@@ -88,6 +92,22 @@ if ip:
     select_by_ip(ip)
 
 
+# Check if ip exist in table ip2drop
+# ------------------------------------------------------------------------------------------------------/
+def ip_exist(ip):
+    conn = connect_db()
+    response = conn.execute("SELECT EXISTS(SELECT 1 FROM ip2drop WHERE ip=?)", (ip,))
+    fetched = response.fetchone()[0]
+    if fetched == 1:
+        # print("Exist")
+        conn.close()
+        return True
+    else:
+        # print("Does not exist")
+        conn.close()
+        return False
+
+
 # Update record in table ip2drop by count where count not equal 0
 # ------------------------------------------------------------------------------------------------------/
 def update_by_count(ip_count):
@@ -100,3 +120,95 @@ def update_by_count(ip_count):
 if count:
     print("Count: " + count)
     update_by_count(count)
+
+
+# Select drop_date from table ip2drop by ip
+# ------------------------------------------------------------------------------------------------------/
+def select_drop_date_by_ip(ip):
+    fetched_date = ""
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("SELECT drop_date FROM ip2drop WHERE ip = ?", (ip,))
+    # print(c.fetchall())
+
+    # Iterate over list c.fetchall()
+    # ---------------------------------/
+    for row in c.fetchall():
+        fetched_date = row[0]
+    return fetched_date
+
+
+# Select timeout from table ip2drop by ip
+# ------------------------------------------------------------------------------------------------------/
+def select_timeout_by_ip(ip):
+    fetched_date = ""
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("SELECT timeout FROM ip2drop WHERE ip = ?", (ip,))
+    # print(c.fetchall())
+    for row in c.fetchall():
+        fetched_date = row[0]
+    return fetched_date
+
+
+drop_date = select_drop_date_by_ip(ip)
+timeout = select_timeout_by_ip(ip)
+
+print(f'Drop date: {drop_date}, Timeout: {timeout}')
+
+
+# Increment record to table ip2drop by ip
+# Increment count by +1
+# ------------------------------------------------------------------------------------------------------/
+def increment_by_ip(ip):
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("""UPDATE ip2drop SET COUNT = COUNT + 1 WHERE ip = ?""", (ip,))
+    conn.commit()
+
+
+# Check date less than or more than current date
+# ------------------------------------------------------------------------------------------------------/
+def check_date(drop_date, timeout):
+    current_date = datetime.now()
+    bool_status = False
+
+    # Convert list to string
+    # ---------------------------------/
+    drop_date = str(drop_date)
+    timeout = str(timeout)
+    # Convert string to datetime
+    # ---------------------------------/
+    drop_date_as_dt = datetime.strptime(drop_date, DATETIME_DEFAULT_FORMAT)
+    timeout_as_dt = datetime.strptime(timeout, DATETIME_DEFAULT_FORMAT)
+
+    print(f'Drop date: {drop_date}, Timeout: {timeout}, Current date: {current_date}')
+
+    if current_date > timeout_as_dt:
+        print("Timeout more than current date. Need ban again")
+        bool_status = True
+    else:
+        print("Timeout less than current date. No need action")
+
+    # Time delta
+    # ------------------------------------------------------------------------------------------------------/
+
+    # String to datetime
+    timeout = datetime.strptime(timeout, DATETIME_DEFAULT_FORMAT)
+    delta = timeout - current_date
+
+    print(delta)
+    # print(delta.days)
+    # print(delta.seconds)
+    # print(delta.microseconds)
+    # print(delta.total_seconds())
+
+    return bool_status
+
+
+if ip_exist(ip):
+    if check_date(drop_date, timeout):
+        print("Need ban again")
+        increment_by_ip(ip)
+else:
+    lib.msg_info(f'IP {ip} not exist in table ip2drop')
