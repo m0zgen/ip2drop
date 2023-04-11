234073ed14fbb5f2842ae95d376fcff5502a9c12
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-04-11 18:45:39 +0600

    + iterate_all_ips - checking dates (drop, timeout) dirrectly from DB ip selection

--- helper.py
+++ helper.py
@@ -26,11 +26,11 @@ DATETIME_DEFAULT_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 # Arguments parser section
 # ------------------------------------------------------------------------------------------------------/
 parser_helper = argparse.ArgumentParser(description='IP2DROP helper')
-parser_helper.add_argument('-hp', '--helper-print', help='Show all records from table ip2drop', default=False, action='store_true')
-parser_helper.add_argument('-hr', '--helper-increment', help='Increment count by 1', default=False, action='store_true')
-parser_helper.add_argument('-hs', '--helper-show', help='Show info', default=False, action='store_true')
-parser_helper.add_argument('-hi', '--helper-ip', help='Get IP address info')
-parser_helper.add_argument('-hc', '--helper-count', help='Reset Count')
+parser_helper.add_argument('-p', '--print', help='Show all records from table ip2drop', default=False, action='store_true')
+parser_helper.add_argument('-r', '--increment', help='Increment count by 1', default=False, action='store_true')
+parser_helper.add_argument('-s', '--show', help='Show info', default=False, action='store_true')
+parser_helper.add_argument('-i', '--ip', help='Get IP address info')
+parser_helper.add_argument('-c', '--count', help='Reset Count')
 args_helper = parser_helper.parse_args()
 print_all = args_helper.print
 ip = args_helper.ip
@@ -66,9 +66,6 @@ def print_all_tables():
     print(c.fetchall())
 
 
-print_all_tables()
-
-
 # Show all records from table ip2drop
 # ------------------------------------------------------------------------------------------------------/
 def show_all_records():
@@ -94,11 +91,11 @@ def ip_exist(ip):
     response = conn.execute("SELECT EXISTS(SELECT 1 FROM ip2drop WHERE ip=?)", (ip,))
     fetched = response.fetchone()[0]
     if fetched == 1:
-        print("Exist")
+        # print("Exist")
         conn.close()
         return True
     else:
-        print("Does not exist")
+        # print("Does not exist")
         conn.close()
         return False
 
@@ -190,6 +187,8 @@ def check_date(ip, drop_date, timeout):
     return bool_status
 
 
+print_all_tables()
+
 if if_show:
     show_info()
 
@@ -211,7 +210,7 @@ if ip:
 drop_date = get_drop_date_from_ip(ip)
 timeout = get_timeout_from_ip(ip)
 
-print(f'Drop date: {drop_date}, Timeout: {timeout}')
+# print(f'Drop date: {drop_date}, Timeout: {timeout}')
 
 if ip_exist(ip):
     if check_date(ip, drop_date, timeout):
@@ -222,3 +221,30 @@ if ip_exist(ip):
 
 else:
     lib.msg_info(f'IP {ip} not exist in table ip2drop')
+
+
+# Iterate all ips from table ip2drop
+# ------------------------------------------------------------------------------------------------------/
+def iterate_all_ips():
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("SELECT * FROM ip2drop")
+    # print(c.fetchall())
+
+    # Iterate over list c.fetchall()
+    # ---------------------------------/
+    for row in c.fetchall():
+        ip = row[0]
+        drop_date = row[5]
+        timeout = row[4]
+        count = row[3]
+        print(f'IP: {ip}, Drop date: {drop_date}, Timeout: {timeout}, Count: {count}')
+
+        if check_date(ip, drop_date, timeout):
+            lib.msg_info(f'IP {ip} need ban again')
+            if if_increment:
+                lib.msg_info(f'Increment count by 1 for IP {ip} in table ip2drop')
+                increment_by_ip(ip)
+
+
+iterate_all_ips()
