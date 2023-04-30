21248bc05a537f75be3152101f1cd8bc44e70574
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-04-30 12:21:08 +0600

    + example daemon routine

--- ip2dropd.py
+++ ip2dropd.py
@@ -1,1 +1,21 @@
 # Initial file for multithreading daemon
+import datetime
+import time
+import daemon
+
+
+def main_routine():
+    while True:
+        with open('/tmp/echo.txt', 'a') as fh:
+            fh.write("{}\n".format(datetime.datetime.now()))
+        time.sleep(1)
+
+
+def main():
+    with daemon.DaemonContext():
+        main_routine()
+
+
+# Init starter
+if __name__ == "__main__":
+    raise SystemExit(main())
