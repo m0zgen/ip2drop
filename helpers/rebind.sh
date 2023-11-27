aa809db9b9c575f9f066269f8c07cf2e7b01cc7e
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-11-27 21:38:54 +0600

    + + update rebind ipset deletion

--- helpers/rebind.sh
+++ helpers/rebind.sh
@@ -33,7 +33,7 @@ fi
 
 # Once operation
 rebind_firewalld(){
-  firewall-cmd --permanent --remove-source=ipset:${IPSET_NAME} --zone=dns
+  firewall-cmd --permanent --remove-source=ipset:${IPSET_NAME} --zone=drop
   firewall-cmd --permanent --delete-ipset=${IPSET_NAME}
   firewall-cmd --reload
   firewall-cmd --permanent --new-ipset=${IPSET_NAME} --type=hash:ip --option=timeout=60  --option=maxelem=524288
